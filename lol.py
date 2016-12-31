1d73023828afe8a2b4d5a511926b33050a382ec9
Leon Phelps <leon.phelps@am.sony.com>
2016-12-30 23:34:45 -0500

    Major fixes
    cleaned the code up a lot and removed the print from within the function.  Now it properly returns the data to be printed later on.

--- lol.py
+++ lol.py
@@ -1,4 +1,4 @@
-#pull stats via riot api
+#Pull some ranked data for a specific summoner via riot api
 
 import json # imports json libraries
 import requests #imports requests libraries
@@ -8,30 +8,13 @@ script, summoner_name_case = argv
 
 summoner_name = summoner_name_case.lower()
 
-
 api_key = "api_key=RGAPI-e11bfdbe-75e7-4a05-961e-6c1d441fbfdd"
-
-def walkdict(d):
-    stack = d.items()
-    while stack:
-        k, v = stack.pop()
-        if isinstance(v, dict):
-            stack.extend(v.iteritems())
-        else:
-            print("%s: %s" % (k, v))
-
 
 def summoner_lookup(summoner_name):
     r = requests.get('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + summoner_name, params=api_key)
     results = r.json()
     sid = results[summoner_name]['id']
     return str(sid)
-
-
-#def print_goodies():
-    #print "This shitter is %s %s in %s" % (sqt, sqd, sq)
-    #print "This shitter is %s %s in %s" % (fqt, fqd, fq)
-
 
 def summoner_rank():
     summoner_id = summoner_lookup(summoner_name)
@@ -44,22 +27,13 @@ def summoner_rank():
     fq = results[summoner_id][1]['queue']
     fqt = results[summoner_id][1]['tier']
     fqd = results[summoner_id][1]['entries'][0]['division']
-    print "This shitter is %s %s in %s" % (sqt, sqd, sq)
-    print "This shitter is %s %s in %s" % (fqt, fqd, fq)
     return (sq, sqt, sqd, fq, fqt, fqd)
 
-def print_func():
-    print(summoner_rank())[0:1]
 
 print "Looking up information for summoner : %s" % summoner_name
-
 print "\n"
-#print_goodies()
 
-#print_func()
-summoner_rank()
+sq, sqt, sqd, fq, fqt, fqd = summoner_rank()
 
-#print "This shitter is %s %s in %s" % (self.sqt, sqd, sq)
-#print "This shitter is %s %s in %s" % (fqt, fqd, fq)
-
-#walkdict(results)
+print "This shitter is %s %s in %s" % (sqt, sqd, sq)
+print "This shitter is %s %s in %s" % (fqt, fqd, fq)
